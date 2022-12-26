503f9d075aeb1cea1ecc69ab6f25b2d0fd7b9bec
Marcus André <andremarcus@johndeere.com>
2022-12-26 08:36:58 +0100

    plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,6 +12,12 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- util
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
   -- lsp
   use {
     'VonHeikemen/lsp-zero.nvim',
@@ -35,39 +41,46 @@ packer.startup(function(use)
     }
   }
 
-  use 'junegunn/gv.vim'
-  use 'mbbill/undotree'
-  use 'mhinz/vim-signify'
+  use 'j-hui/fidget.nvim'
+
+  -- treesitter
   use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use 'nvim-lualine/lualine.nvim'
-  use 'sgur/vim-editorconfig'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-projectionist'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'vim-test/vim-test'
-
-
-  use {
-    'mrjones2014/dash.nvim',
-    run = 'make install',
-  }
 
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
   }
 
+  -- git
+  use 'junegunn/gv.vim'
+  use 'mhinz/vim-signify'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+
+  -- ui
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'nvim-lualine/lualine.nvim'
+
+  -- workflow
+  use 'mbbill/undotree'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-projectionist'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-sleuth'
+  use 'tpope/vim-surround'
+  use 'vim-test/vim-test'
+  -- use('theprimeagen/harpoon')
+  -- use 'sgur/vim-editorconfig'
+
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
+    'mrjones2014/dash.nvim',
+    run = 'make install',
   }
 
   -- themes
   use 'archseer/colibri.vim'
   use 'arcticicestudio/nord-vim'
-  use 'f-person/auto-dark-mode.nvim'
   use 'cocopon/iceberg.vim'
+  use 'f-person/auto-dark-mode.nvim'
+  use 'folke/tokyonight.nvim'
 end)
