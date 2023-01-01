6a417535c68931bd5df12fb89c6feee85bc7a62f
Marcus André <hello+github@marcusandre.de>
2023-01-01 16:54:35 +0100

    packer: add bootstrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,15 @@
--- Testing
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 local status, packer = pcall(require, "packer")
 
@@ -83,4 +94,8 @@ packer.startup(function(use)
   use 'cocopon/iceberg.vim'
   use 'f-person/auto-dark-mode.nvim'
   use 'folke/tokyonight.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
